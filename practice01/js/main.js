106b79524f2b96f18ed59ad8a1bb1e800748be87
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-04-22 20:31:10 +0900

    section02 v-bind v-on

--- practice01/js/main.js
+++ practice01/js/main.js
@@ -1,11 +1,31 @@
 new Vue({
   el: '#app',
   data: {
-    message: 'Hello World!'
+    message: 'Hello World!',
+    number: 0,
+    ok: false,
+    html: '<h1>h1です</h1>',
+    url: 'https://www.google.com/?hl=ja',
+    x: 0,
+    y: 0,
+    event: 'click'
   },
   methods: {
     reverseMessage: function() {
       this.message = this.message.split('').reverse().join('');
+    },
+    sayHi: function() {
+      return this.message;
+    },
+    countUp: function() {
+      this.number += 1;
+    },
+    changeMousePosition: function(event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
+    },
+    myAlert: function() {
+      alert('アラート！');
     }
   }
 });
