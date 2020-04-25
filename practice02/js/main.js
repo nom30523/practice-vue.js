f7ab13b17e583b5ea110e4511b115c4f509e622f
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-04-25 20:32:07 +0900

    v-for

--- practice02/js/main.js
+++ practice02/js/main.js
@@ -2,6 +2,17 @@ new Vue({
   el: '#app',
   data: {
     ok: false,
-    maybeOk: true
+    maybeOk: true,
+    fruits: ['りんご', 'バナナ', 'ぶどう'],
+    object: {
+      firstName: '太郎',
+      lastName: '田中',
+      age: 27
+    }
+  },
+  methods: {
+    remove: function() {
+      this.fruits.shift();
+    }
   }
 });
