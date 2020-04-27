96d0c6b667731696f720938e8aa0546ec7e1d174
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-04-27 21:01:51 +0900

    external access

--- practice03/js/main.js
+++ practice03/js/main.js
@@ -1,13 +1,20 @@
-new Vue({
+var vm1 = new Vue({
   el: '#app1',
   data: {
     message: 'インスタンス１'
   }
 });
 
-new Vue({
+vm1.message = '書き換えられました'
+
+var vm2 = new Vue({
   el: '#app2',
   data: {
     message: 'インスタンス２'
+  },
+  methods: {
+    changeMessage: function() {
+      vm1.message = 'インスタンス２から変えました'
+    }
   }
 });
